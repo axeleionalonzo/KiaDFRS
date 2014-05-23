d740b26953ceda007fd45dd4634b1edf5b182bf7
unknown <alonzo.axeleion@gmail.com>
2014-05-23 11:57:30 +0800

    initial progress google chart

--- application/views/consultant/consultantview.php
+++ application/views/consultant/consultantview.php
@@ -12,6 +12,43 @@ $is_logged_in = $this->session->userdata('is_logged_in');
     <link href="<?php echo base_url();?>css/bootstrap.css" media="screen" rel="stylesheet" type="text/css" />
     <link href="<?php echo base_url();?>css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
 
+    <script type="text/javascript" src="//www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load('visualization', '1', {packages: ['corechart']});
+    </script>
+    <script type="text/javascript">
+      function drawVisualization() {
+        // Create and populate the data table.
+        var data = google.visualization.arrayToDataTable([
+          ['x', 'Cats', 'Blanket 1', 'Blanket 2'],
+          ['A',   1,       1,           0.5],
+          ['B',   2,       0.5,         1],
+          ['C',   4,       1,           0.5],
+          ['D',   8,       0.5,         1],
+          ['E',   7,       1,           0.5],
+          ['F',   7,       0.5,         1],
+          ['G',   8,       1,           0.5],
+          ['H',   4,       0.5,         1],
+          ['I',   2,       1,           0.5],
+          ['J',   3.5,     0.5,         1],
+          ['K',   3,       1,           0.5],
+          ['L',   3.5,     0.5,         1],
+          ['M',   1,       1,           0.5],
+          ['N',   1,       0.5,         1]
+        ]);
+      
+        // Create and draw the visualization.
+        new google.visualization.LineChart(document.getElementById('visualization')).
+            draw(data, {curveType: "function",
+                        width: 500, height: 400,
+                        vAxis: {maxValue: 10}}
+                );
+      }
+      
+
+      google.setOnLoadCallback(drawVisualization);
+    </script>
+
 </head>
 <body>
 <div class="container">
@@ -30,9 +67,11 @@ $is_logged_in = $this->session->userdata('is_logged_in');
           <p>
             <img src="<?php echo base_url();?>img/default_profile.jpg" height="214" width="214"/>
           </p>
+            <?php if (($query[0]['username'])==($username)) { ?>
             <form enctype="multipart/form-data" action="image.php" method="POST">
               <input type="file" id="exampleInputFile" />
             </form>
+            <?php } ?>
         </div>
       </div>
       <div class="col-md-9 column"><br><br><br><br><br>
@@ -43,6 +82,7 @@ $is_logged_in = $this->session->userdata('is_logged_in');
           <span class="help-block"></font></span>
         </div>
       </div>
+      <?php if (($query[0]['username'])==($username)) { ?>
       <div class="form-group">
         <label class="col-lg-2 control-label">Password</label>
         <div class="col-lg-10">
@@ -50,11 +90,22 @@ $is_logged_in = $this->session->userdata('is_logged_in');
           <span class="help-block"></font></span>
         </div>
       </div>
+      <?php } ?>
       <div class="form-group">
         <label class="col-lg-2 control-label">Reports Made</label>
         <div class="col-lg-10">
           <input disabled="" type="text" class="form-control" value="<?php echo count($recordsbyconsultatnt); ?>">
           <span class="help-block"></font></span>
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-lg-2 control-label">Progress</label>
+        <div class="col-lg-10">
+          <?php for ($i=0;$i<count($recordsbyconsultatnt);$i++) { 
+            echo $recordsbyconsultatnt[$i]->client;
+            echo $recordsbyconsultatnt[$i]->report_date;
+          }?>
+          <div id="visualization" style="width: 500px; height: 400px;"></div>
         </div>
       </div>
       </div>
